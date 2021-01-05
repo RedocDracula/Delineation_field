55b920446383e7d2d65f2469e88e9f73d25636d7
Shreekanth Ajith <37432767+RedocDracula@users.noreply.github.com>
2021-01-05 13:12:12 +0530

    Update preprocessing.py

--- ForAfric-Agricultural-Fields-Delineation-master/preprocessing.py
+++ ForAfric-Agricultural-Fields-Delineation-master/preprocessing.py
@@ -45,14 +45,15 @@ import utils.coco
 
 """## Vector preparation """
 
-inpath_s2 = Path(r'path to .tif file ')
-inpath_fields = Path(r'path to .shp file')
+
+inpath_s2 = Path(r'data/RGB_small.tif')
+inpath_fields = Path(r'data/marker2016_small.shp')
 
 outpath = Path(r'output/preprocessed')
 
 def prepare_vector(fp, out_crs, clipping_bounds):
     df = (gpd.read_file(str(fp), encoding='cp865')  # danish encoding
-             .rename(columns={'Afgroede': 'lc_name', 'AfgKode': 'lc_id', 'JOURNALNUM': 'journalnr'}) #check for the keys before preprocessing , else key errors will come
+             .rename(columns={'Afgroede': 'lc_name', 'AfgKode': 'lc_id', 'JOURNALNUM': 'journalnr'})
              .drop(['GB', 'GEOMETRISK', 'MARKNUMMER'], axis=1)
              .pipe(utils.geo.explode_mp)
              .pipe(utils.geo.buffer_zero)
@@ -87,13 +88,8 @@ else:
 print(df.info())
 display(df.head(3))
 
-"""## Cut to chips
-
-### Clip vectors to chip geometries
-"""
-
 outpath_chips = outpath / 'chip_dfs.pkl'
-chip_width, chip_height = 128, 128 #input size for image 
+chip_width, chip_height = 128, 128
 
 if not outpath_chips.exists():
     with rasterio.open(inpath_s2) as src:
@@ -111,15 +107,12 @@ else:
     
 print('len', len(chip_dfs))
 
-"""### Convert chip vectors to COCO annotation format (contains test/test split) and export."""
-
 train_chip_dfs, val_chip_dfs = utils.coco.train_test_split(chip_dfs, test_size=0.2, seed=1)
 coco_train = utils.coco.format_coco(train_chip_dfs, chip_width, chip_height)
 coco_val = utils.coco.format_coco(val_chip_dfs, chip_width, chip_height)
 new_json(outpath / r'annotations/train2016.json', coco_train)
 new_json(outpath / r'annotations/val2016.json', coco_val)
 
-"""### Crop images to chips"""
 
 # Cut to chip images and write to disk, retrieve chip image statistics
     
@@ -130,16 +123,10 @@ chip_statistics = utils.img.cut_chip_images(inpath_raster=inpath_s2,
                                             chip_windows=chip_windows.values(), 
                                             bands=[3, 2, 1])
 
-
 utils.coco.move_coco_val_images(inpath_train_folder=outpath / r'images/train2016',
                                 val_chips_list=val_chip_dfs.keys())
 
 print('len', len(chip_statistics))
-
-
-"""## Save statistics & visualize results(optional)
-The statistics are partially used as model training parameters.
-"""
 
 statistics = {
     'nr_chips': len(chip_dfs.keys()),
@@ -153,8 +140,16 @@ statistics = {
     'train_rgb_std': list(np.asarray([df['std'] for df in [chip_statistics[key] for key in train_chip_dfs.keys()]]).mean(axis=0))}
 
 new_json(outpath / 'statistics.json', statistics)
-print(statistics)
+pprint(statistics)
 
 utils.coco.plot_coco(inpath_json=outpath / r'annotations/val2016.json', 
                      inpath_image_folder=outpath / r'images/val2016', 
                      end=2)
+
+
+
+
+
+
+
+
